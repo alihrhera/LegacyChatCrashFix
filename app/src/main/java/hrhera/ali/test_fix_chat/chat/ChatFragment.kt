4884aed580d3202f71cf75642f4fde0441d6269c
Ali hrera <alihrhera1@gmail.com>
2025-09-18 02:16:26 +0300

    Refactor: Initialize MessagesAdapter in onViewCreated
    The `MessagesAdapter` is now initialized and set to the `RecyclerView` within the `onViewCreated` lifecycle method instead of `onCreateView`.
    
    This change ensures that the adapter is set up after the view hierarchy has been fully inflated, preventing potential null pointer exceptions related to view binding. Additionally, the `adapter` variable is now declared within `onViewCreated` as it is only used locally within that method, improving encapsulation.

--- app/src/main/java/hrhera/ali/test_fix_chat/chat/ChatFragment.kt
+++ app/src/main/java/hrhera/ali/test_fix_chat/chat/ChatFragment.kt
@@ -5,15 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import hrhera.ali.test_fix_chat.databinding.FragmentChatBinding
 import kotlin.getValue
 
 class ChatFragment : Fragment() {
-    private lateinit var binding: FragmentChatBinding
-    private var adapter = MessagesAdapter(emptyList())
-    private val viewModel by viewModels<ChatViewModel>()
+    lateinit var binding: FragmentChatBinding
+    val viewModel by viewModels<ChatViewModel>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -25,7 +23,6 @@ class ChatFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         super.onCreateView(inflater, container, savedInstanceState)
         binding = FragmentChatBinding.inflate(inflater, container, false)
-        binding.messages.adapter = adapter
         return binding.root
     }
 }
